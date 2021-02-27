9da1e1f344171bfd437fdd9134c1fa068eb52b69
Michael Dreesen <michaeldreesen90@gmail.com>
2021-02-27 10:30:34 -0500

    added images and navbar to the site

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 // import pages
 import Home from './pages/Home';
 
+// import components
+import Navbar from './components/Navbar';
+
 function App() {
   return (
     <Router>
       <div className="App">
+      <Navbar />
         <Route exact path="/" component={Home} />
       </div>
     </Router>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 
 function Home() {
     return(
-        <div>Home</div>
+        <div>Home
+            <section className="topSection">
+                <div>Testing</div>
+                <div className="imgContainer"></div>
+            </section>
+        </div>
     );
 }
 
