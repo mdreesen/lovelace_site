d661308bc75191688c027559cbf4b2df88a1e453
Michael Dreesen <michaeldreesen90@gmail.com>
2021-02-27 13:04:28 -0500

    Added text and navbar link alignment

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,22 +10,22 @@ const Navbar = () => {
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          <div className="navbar-link-container collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link" aria-current="page" href="/">home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="/about">about</a>
+                <a className="nav-link" href="/about">about</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="/services">services</a>
+                <a className="nav-link" href="/services">services</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="/appointments">appointments</a>
+                <a className="nav-link" href="/appointments">appointments</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="/contact">contact</a>
+                <a className="nav-link" href="/contact">contact</a>
               </li>
             </ul>
           </div>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,10 +2,19 @@ import React from 'react';
 
 function Home() {
     return(
-        <div>Home
+        <div>
             <section className="topSection">
-                <div>Testing</div>
                 <div className="imgContainer"></div>
+            </section>
+            <section>
+                <div className="about-business">
+                <h3 className="mission-header">Mission</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
+                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
+                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
+                    mollit anim id est laborum.</p>
+                </div>
             </section>
         </div>
     );
