2d3fed1b4016745e7516bbfc4f991f381d82f359
Michael Dreesen <michaeldreesen90@gmail.com>
2021-02-27 11:05:20 -0500

    Hooking up pages for react router

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // import pages
 import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Appointments from './pages/Appointment';
+import Contact from './pages/Contact';
 
 // import components
 import Navbar from './components/Navbar';
@@ -14,6 +18,10 @@ function App() {
       <div className="App">
       <Navbar />
         <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About}/>
+        <Route exact path="/services" component={Services}/>
+        <Route exact path="/appointments" component={Appointments}/>
+        <Route exact path="/contact" component={Contact}/>
       </div>
     </Router>
   );
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light">
         <div className="container-fluid">
         {/*
           <a className="navbar-brand" href="#">Navbar</a>
@@ -13,19 +13,19 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active" aria-current="page" href="/">Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">about</a>
+                <a className="nav-link active" href="/about">about</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">services</a>
+                <a className="nav-link active" href="/services">services</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">appointments</a>
+                <a className="nav-link active" href="/appointments">appointments</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link active" href="#">contact</a>
+                <a className="nav-link active" href="/contact">contact</a>
               </li>
             </ul>
           </div>
