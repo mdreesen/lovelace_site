34c1b0899d44b34159abcda351625600d34e7bf0
Michael Dreesen <michaeldreesen90@gmail.com>
2021-03-04 22:07:19 -0500

    made about and services text

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 
 const About = () => {
     return(
-        <div>About Page</div>
+        <div>
+        <div className="brianna-image-container">Image Here</div>
+            <p className="about-text-hello">Hello!</p>
+            <p className="about-text">I am Brianna Lovelace. My birth work journey started a little over 3 years ago when I was pregnant with my first child. We hired her not fully sure of the decision or understanding her worth in that moment. I went on to birth my son with no pain medicine in the hospital with a midwife. My doula was was phenomenal and helped the labor and birth to go smoothly. I remember telling her at our postpartum visit that I thought I wanted to be a doula. I wanted to help women achieve the birth of their dreams. But as soon as the thought came, it left. I had a newborn and starting a new career, business, etc just wasn’t in the cards.
+
+Fast forward a year and I’m pregnant with my second baby. I went about this pregnancy and birth quite different than my first. I chose to have a wild pregnancy and free birth. While there were a few moments of medical assistance thrown in there, I largely depended on my own research and innate wisdom throughout the process. I opted out of hiring a doula for a variety of reasons. That ultimately was my biggest regret of that pregnancy and labor. Nothing went wrong at all. In fact, everything went exactly as planned. But I was missing something.
+
+It was after this birth that my fire was lit again to jump into the birth world! I opened my business in the spring of 2019.
+
+I started an online certification program and found out very quickly that it was not for me at all. I didn’t align with the teachings and the teachings were very superficial. I realized that I didn’t want to just know how to support women. I want to know how to be with women in whatever way I was needed.
+
+I continued to self educate myself while working until learned about Indie Birth’s doula training in the fall of 2019. I went through this program and loved every minute of it. We dove deep into heart work, what it meant to be with women, how to hold space and so much more. This program stated that it purposely did not certify its trainees because women do not need to be certified to know how to be with women. I realized that resonated so deeply. I am grateful for the lessons that I learned in that training and am proud to be able to have an education, not just a certification.
+
+Through my work and training I learned I have a little bit of a different philosophy about birth than many do. I believe in physiological birth. I believe that birth should be undisturbed by unnecessary medical interventions. Birth that is mother led. The birthing person knows better than anyone else what she needs and doesn’t need.
+
+As I’ve continued my doula work and being a woman of color, the health disparities associated with our pregnancies and births became overwhelmingly obvious. Part of my job has now transitioned to be that advocate for the WOC who want it. I decided to take Uzazi Village’s perinatal doula training to be better equipped.
+
+My passion for birth morphed into my dream career. I’m so happy you’re here learning more about me and hope to connect with you soon!</p>
+        </div>
     );
 }
 
--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,8 +1,57 @@
 import React from 'react';
 
+import ServiceModal from '../../components/ServiceModal';
+
 const Services = () => {
     return(
-        <div>Services Page</div>
+        <div>
+            <section>
+                <div className="service-p-container">
+                <p>Labor Support-$850 -> Demo 1</p>
+                <ServiceModal />
+            </div>
+        </section>
+        <section className="service-type-section-demo2">
+            <div className="service-p-container">
+                <h4>Virtual Support-$650 -> Demo 2</h4>
+                <p className="p-title">• 2 prenatal visits ranging from 1-2 hours each that include:</p>
+                <p>o health history</p>
+                <p>o questions/concerns</p>
+                <p>o mental health screening</p>
+                <p>o provider questions</p>
+                <p>o birth plan formation</p>
+                <p className="p-title">• Labor support •</p>
+                <p>o Placement on my calendar</p>
+                <p>o 24/7 on call support from two weeks before due date to two weeks postpartum</p>
+                <p className="p-title">• Postpartum Support •</p>
+                <p>o 1 2 hour virtual visit for debriefing</p>
+            </div>
+        </section>
+        <section className="service-type-section-demo3">
+            <div className="service-p-container">
+                <h4>Prenatal lactation education class-$75 (1 hour) -> Demo 3</h4>
+                <p>o will be group or private depending on number of participants</p>
+                <p>o common breastfeeding myths</p>
+                <p>o proper latch techniques</p>
+                <p>o newborn breastfeeding through toddlerhood breastfeeding </p>
+            </div>
+        </section>
+        <section className="service-type-section-demo2">
+            <div className="service-p-container-demo4">
+                <h4>Childbirth education class-$200 (1.5 hrs) -> Demo 4</h4>
+                <p className="demo-p4">o Group setting</p>
+                <p className="demo-p4">o What is physiological birth</p>
+                <p className="demo-p4">o Hormones of birth</p>
+                <p className="demo-p4">o What does undisturbed broth look like</p>
+            </div>
+        </section>
+        <section>
+            <div>
+                <h4>Placenta encapsulation-$250 -> Same as demo 2</h4>
+                <p>o Will provide encapsulated pills in container within 3 days of birth unless otherwise discussed</p>
+            </div>
+        </section>
+        </div>
     );
 }
 
