a8e00a97be294af53da71d5a0756ea39696462b4
Michael Dreesen <michaeldreesen90@gmail.com>
2021-04-22 23:36:57 -0400

    getting rid of CSS that is distorting the images

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,20 +10,21 @@ function Home() {
             <section className="topSection">
                 <div className="image-wrapper-home">
                     <div className="imgContainer">
+                    <img className="logo-image" src={logo}></img>
+
                         <img className="homepageImage" src={homepageImage}></img>
-                        <img className="logo-image" src={logo}></img>
                     </div>
                 </div>
             </section>
             <section>
-                <div className="about-business">
+                {/* <div className="about-business">
                 <h2 className="mission-header">Mission</h2>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
                     dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip 
                     ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu 
                     fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt 
                     mollit anim id est laborum.</p>
-                </div>
+                </div> */}
             </section>
         </div>
     );
