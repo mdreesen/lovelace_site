451a5fed5ce993e67153bc402c1f619aef460591
Michael Dreesen <michaeldreesen90@gmail.com>
2021-06-03 22:44:25 -0400

    getting text and images on the about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // import images
-// import briannaImage from '../../images/briannaImage.jpeg';
+import familyImage from '../../images/familyImage.jpeg';
 
 const About = () => {
     return (
@@ -13,25 +13,46 @@ const About = () => {
                 <div className="about-section-two">
                 <div className="about-text-container">
                     <div className="about-para-container">
-                        <h3 className="brianna-name-title">Brianna Lovelace</h3>
-                        <p className="about-text-one">I am Brianna Lovelace. My birth work journey started a little over 3 years ago when I was pregnant with my first child. We hired her not fully sure of the decision or understanding her worth in that moment. I went on to birth my son with no pain medicine in the hospital with a midwife.
-                        My doula was was phenomenal and helped the labor and birth to go smoothly.
-                        I remember telling her at our postpartum visit that I thought I wanted to be a doula. I wanted to help women achieve the birth of their dreams. But as soon as the thought came, it left. I had a newborn and starting a new career, business, etc just wasn’t in the cards.</p>
+                    <div className="qualification-container">
+                        <h3 className="brianna-name-title">Hello, I’m Brianna Lovelace!</h3>
+                        <h4 className="qualification">Doula Certificate</h4>
+                        <h4 className="qualification">Childbirth Educator Certificate</h4>
+                        <h4 className="qualification">Lactation Specialist Certificate</h4>
+                        <h4 className="qualification">Baby Wearing Educator Certificate</h4>
+                        <h4 className="qualification">Placenta Specialist Certificate</h4>
                     </div>
-                    <p className="about-text-two">Fast forward a year and I’m pregnant with my second baby. I went about this pregnancy and birth quite different than my first. I chose to have a wild pregnancy and free birth. While there were a few moments of medical assistance thrown in there, I largely depended on my own research and innate wisdom throughout the process. 
-                    I opted out of hiring a doula for a variety of reasons. That ultimately was my biggest regret of that pregnancy and labor. Nothing went wrong at all. In fact, everything went exactly as planned. But I was missing something.
-                    It was after this birth that my fire was lit again to jump into the birth world! I opened my business in the spring of 2019.
-                    </p>
-                    <p className="about-text-three">I started an online certification program and found out very quickly that it was not for me at all. I didn’t align with the teachings and the teachings were very superficial. I realized that I didn’t want to just know how to support women. I want to know how to be with women in whatever way I was needed.
-                    I continued to self educate myself while working until learned about Indie Birth’s doula training in the fall of 2019. I went through this program and loved every minute of it. We dove deep into heart work, what it meant to be with women, how to hold space and so much more. 
-                    This program stated that it purposely did not certify its trainees because women do not need to be certified to know how to be with women. 
-                    I realized that resonated so deeply. I am grateful for the lessons that I learned in that training and am proud to be able to have an education, not just a certification.
-                    </p>
-                    <p className="about-text-four">
-                        Through my work and training I learned I have a little bit of a different philosophy about birth than many do. I believe in physiological birth. I believe that birth should be undisturbed by unnecessary medical interventions. Birth that is mother led. The birthing person knows better than anyone else what she needs and doesn’t need.
-                        As I’ve continued my doula work and being a woman of color, the health disparities associated with our pregnancies and births became overwhelmingly obvious. Part of my job has now transitioned to be that advocate for the WOC who want it. I decided to take Uzazi Village’s perinatal doula training to be better equipped.
-                        My passion for birth morphed into my dream career. I’m so happy you’re here learning more about me and hope to connect with you soon!
-                    </p>
+
+                        <p className="about-text-one">I have a heart to support birthing and postpartum mothers. 
+                        After marrying my highschool sweetheart, earning my Bachleor’s degree in psychology and becoming pregnant 
+                        with our first child, I became interested in becoming a doula. 
+                        I later trained at the Indie Birth Association, which emphasizes autonomous and natural birthing practices, 
+                        as well as Uzazi Village, which prioritizes birth assistance for black and brown families. 
+                        These Associations have given me unique experiences to better serve families. 
+                        I’m so grateful for my supportive husband, with whom I now have 3 young children, 
+                        or encouraging me to pursue my dreams. In my free time I love to spend time with my family, 
+                        homeschool my children, go to church with my family, read the Word, work on handcrafts, garden, 
+                        explore the outdoors (kids and dirt were made for each other!) and continue my birth work education 
+                        to become a midwife one day.</p>
+
+                        <p className="about-text-two">When I was pregnant with my oldest, 
+                        I was fortunate to have a doula support me through his birth. She was even able to be there in the hospital with me, 
+                        where literally everything went according to plan. Not everyone has that experience, but I am grateful she was there to guide 
+                        the process and advocate for me. For my second birth, I decided I needed to freebirth and I had my baby at home with my husband. 
+                        I had a great experience at home and went on to birth the same way for my third baby.</p>
+
+                        <p className="about-text-three">It was after I had my second child that I finally decided to step out and become a doula. 
+                        I saw that our natural-minded, freebirth & BIPOC communities need more birthing support during this amazing season of pregnancy. 
+                        Black women have a higher c-section rate and both women and their babies have a much higher mortality when not using a doula for support. 
+                        I want to help influence those positive outcomes and support mothers and fathers through the process.</p>
+
+                        <p className="about-text-four">I can’t wait to learn more about you! 
+                        Please reach out to schedule a free consultation so 
+                        that we can better get to know each other & see if we might be a good fit.</p>
+
+                        <div className="family-image-container" >
+                            <img className="family-image" src={familyImage}></img>
+                        </div>
+                    </div>
                 </div>
             </div>
             </div>
