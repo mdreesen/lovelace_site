44ecd65ddcd84dee1d3ad313475a595b04febfbf
Michael Dreesen <michaeldreesen90@gmail.com>
2021-06-04 23:28:15 -0400

    adding to service page. Need to make dropdown and animations

---
+++ src/components/Servicedropdown/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Servicedropdown() {
+    return (
+        <div>this is the service dropdown</div>
+    );
+}
+
+export default Servicedropdown;
--- src/pages/Services/index.js
+++ src/pages/Services/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
 
+// import components
+import Servicedropdown from '../../components/Servicedropdown';
 
 // import pictures
-// import servicehero from '../../images/dexter-chatuluka-OyvZbtXJRlA-unsplash.jpg';
+import servicehero from '../../images/dexter-chatuluka-OyvZbtXJRlA-unsplash.jpg';
 
 const Services = () => {
     return(
         <div>
+        <Parallax className="brianna-image" bgImage={ servicehero } strength={500}></Parallax>
         <div className="service-text-container">
             <p>From Childbirth to lactation support to baby wearing training, 
             I am here to educate and assist expecting and postpartum mothers 
             find care and assistance with perinatal services while respecting their autonomy.
             Bundled packages are eligible for a discount.</p>
         </div>
+
+        <Servicedropdown />
         </div>
     );
 }
